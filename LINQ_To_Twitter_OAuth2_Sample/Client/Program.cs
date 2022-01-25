946f687d3b2137454954dd48ed59f7db9122acee
JeepNL <jdkramer@tutanota.com>
2022-01-25 16:15:18 +0100

    Add project files.

---
+++ LINQ_To_Twitter_OAuth2_Sample/Client/Program.cs
@@ -0,0 +1,11 @@
+using LINQ_To_Twitter_OAuth2_Sample.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
